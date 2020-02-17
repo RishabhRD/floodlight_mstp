d2aac17c1de890905ae534ed40186dfb8e2ac752
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-02-17 21:31:31 +0530

    added initial codes

--- MSTP.java
+++ MSTP.java
@@ -1,14 +1,26 @@
 package net.floodlightcontroller.floodlight_mstp;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFPortConfig;
 import org.projectfloodlight.openflow.protocol.OFPortDesc;
+import org.projectfloodlight.openflow.protocol.OFPortMod;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.types.DatapathId;
+import org.projectfloodlight.openflow.types.OFPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.IOFSwitchListener;
@@ -19,6 +31,10 @@ import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
 
 public class MSTP implements IFloodlightModule ,IOFMessageListener, IOFSwitchListener{
+	protected static Logger log = LoggerFactory.getLogger(MSTP.class);
+	protected IFloodlightProviderService floodlightProviderService;
+	protected int curSeqNumber = 0;
+	protected TreeMap<IOFSwitch,ArrayList<OFPort>> blockedMap;
 
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
@@ -41,12 +57,15 @@ public class MSTP implements IFloodlightModule ,IOFMessageListener, IOFSwitchLis
 	@Override
 	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
 		// TODO Auto-generated method stub
+		floodlightProviderService = context.getServiceImpl(IFloodlightProviderService.class);
+		blockedMap = new TreeMap<>();
 
 	}
 
 	@Override
 	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
 		// TODO Auto-generated method stub
+		floodlightProviderService.addOFMessageListener(OFType.PACKET_IN, this);
 
 	}
 
@@ -77,13 +96,15 @@ public class MSTP implements IFloodlightModule ,IOFMessageListener, IOFSwitchLis
 	@Override
 	public void switchAdded(DatapathId switchId) {
 		// TODO Auto-generated method stub
-
+		unblockAll();
+		startProcess();
 	}
 
 	@Override
 	public void switchRemoved(DatapathId switchId) {
 		// TODO Auto-generated method stub
-
+		unblockAll();
+		startProcess();
 	}
 
 	@Override
@@ -108,6 +129,36 @@ public class MSTP implements IFloodlightModule ,IOFMessageListener, IOFSwitchLis
 	public void switchDeactivated(DatapathId switchId) {
 		// TODO Auto-generated method stub
 
+	}
+	private void startProcess(){
+
+	}
+	private void block(IOFSwitch sw,OFPort port){
+		sendPortMod(sw,port,false);
+	}
+
+	private void unblockAll(){
+		Set<Entry<IOFSwitch,ArrayList<OFPort>>> set = blockedMap.entrySet();
+		Iterator<Entry<IOFSwitch,ArrayList<OFPort>>> itr = set.iterator();
+		while(itr.hasNext()){
+			Entry<IOFSwitch,ArrayList<OFPort>> entry = itr.next();
+			for(OFPort port : entry.getValue()){
+				sendPortMod(entry.getKey(),port,true);
+			}
+		}
+		blockedMap.clear();
+	}
+
+	private void sendPortMod(IOFSwitch sw,OFPort port,boolean enable){
+		OFPortMod.Builder builder = sw.getOFFactory().buildPortMod();
+		HashSet<OFPortConfig> set = new HashSet<>();
+		if(!enable){
+			set.add(OFPortConfig.PORT_DOWN);
+		}
+		HashSet<OFPortConfig> mask = new HashSet<>();
+		mask.add(OFPortConfig.PORT_DOWN);
+		OFPortMod mod  = builder.setPortNo(port).setConfig(set).setMask(mask).setAdvertise(0x00).setHwAddr(sw.getPort(port).getHwAddr()).build();
+		sw.write(mod);
 	}
 
 }
